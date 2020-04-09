10f7af2fd4eb1687972be7b1db2c567c8c4a4190
ljung5 <01032883398a@gmail.com>
2020-04-09 22:29:01 +0900

    008. 생명주기 함수 shouldComponedntUpdate() 사용하기

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import './App.css';
-import Variable from './R010_Variable'
+import LifecycleEx from './R008_LifecycleEx'
 
 function App() {
   return (
     <div>
       <h1>Start React 200!</h1>
       <p>CSS 적용하기</p>
-      <Variable/>
+      <LifecycleEx/>
     </div>
   );
 }
