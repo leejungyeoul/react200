3ccf3f674721618554cc408469b2b736d80c650e
ljung5 <01032883398a@gmail.com>
2020-04-25 00:17:55 +0900

    034. reactstrap Alerts 사용하기

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import './App.css';
-import Jquery from './R016_Jquery'
+import ReactstrapAlerts from './R034_ReactstrapAlerts'
+import 'bootstrap/dist/css/bootstrap.css'
 
 function App() {
   return (
     <div>
       <h1>Start React 200!</h1>
       <p>CSS 적용하기</p>
-      <Jquery/>
+      <ReactstrapAlerts/>
     </div>
   );
 }
