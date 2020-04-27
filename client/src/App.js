440c8766b3b694c0403781c47e37a49e0046581f
ljung5 <01032883398a@gmail.com>
2020-04-28 00:55:51 +0900

    017. props 사용하기

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,13 @@
 import React from 'react';
 import './App.css';
-import ReactstrapBreadcrumbs from './R036_ReactstrapBreadcrumbs'
-import 'bootstrap/dist/css/bootstrap.css'
+import Props from './R017_Props'
 
 function App() {
   return (
     <div>
       <h1>Start React 200!</h1>
       <p>CSS 적용하기</p>
-      <ReactstrapBreadcrumbs/>
+      <Props props_val="THIS IS PROPS"/>
     </div>
   );
 }
