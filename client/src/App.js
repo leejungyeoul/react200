8aee8b79b6e3be368f07b814e17bb2bb4ad54fc6
ljung5 <01032883398a@gmail.com>
2020-05-05 17:33:57 +0900

    027. Component (class형 컴포넌트)

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import './App.css';
-import ShallowEqual from './R029_ShallowEqual'
+import ComponentClass from './R027_ComponentClass'
 
 function App() {
   return (
     <div>
       <h1>Start React 200!</h1>
       <p>CSS 적용하기</p>
-      <ShallowEqual/>
+      <ComponentClass/>
     </div>
   );
 }
