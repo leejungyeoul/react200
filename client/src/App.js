43452653ff90e4df1c380adfc63c814edf347550
ljung5 <01032883398a@gmail.com>
2020-05-17 01:38:03 +0900

    058. sweetalert2 confirm 사용하기

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import Sweetalert2Basic from './R056_Sweetalert2Basic'
+import Sweetalert2Confirm from './R058_Sweetalert2Confirm'
 
 function App() {
   return (
     <div>
       <h1>Start React 200!</h1>
-      <Sweetalert2Basic/>
+      <Sweetalert2Confirm/>
     </div>
   );
 }
