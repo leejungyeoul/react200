06e1b5e6cf13186e195c94956ebc70a12cd8b7cd
ljung5 <01032883398a@gmail.com>
2020-05-25 23:06:17 +0900

    62. aixos post 사용하기

--- client/src/R062_AxiosPost.js
+++ client/src/R062_AxiosPost.js
@@ -1,17 +1,19 @@
 import React, { Component } from 'react';
 import axios from "axios";
 
-class R061_AxiosGet extends Component {
+class R062_AxiosPost extends Component {
   componentDidMount(){
-    axios.get('http://date.jsontest.com/')
+    axios.post('http://date.jsontest.com/', {
+      a:"react", b:200
+    })
     .then( response => {alert(response.data.date)})
   }
 
   render() {
     return (
-        <h1>axios get</h1>
+        <h1>axios post</h1>
     )
   }
 }
 
-export default R061_AxiosGet;
+export default R062_AxiosPost;
