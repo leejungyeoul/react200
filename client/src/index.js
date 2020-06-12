3a8de5f4bb442a2fe1052cd1438ab274d92eec70
ljung5 <01032883398a@gmail.com>
2020-06-13 02:49:42 +0900

    78. redux 리듀서로 스토어 생성 79. redux 스토어 상속와 디스패치 함수 실행 80. redux 리듀서에서 스토어 데이터 변경

--- client/src/index.js
+++ client/src/index.js
@@ -2,14 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
+import {createStore} from 'redux';
+import reducers from './reducers';
 
-ReactDOM.render(
-    <App />,
-  document.getElementById('root')
-);
+const store = createStore(reducers);
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const listener = ()=> {
+    ReactDOM.render(
+        <App store={store}/>,
+        document.getElementById('root')
+    );
+};
+
+store.subscribe(listener);
+listener();
