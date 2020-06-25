d618eab7601874247c2a791002b1b439b7552e9b
ljung5 <01032883398a@gmail.com>
2020-06-25 22:48:45 +0900

    109. react 서버와 node 서버를 프록시로 연결하기

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -13,14 +13,7 @@ import Footer from './Footer/Footer';
 // login
 import LoginForm from './LoginForm';
 
-import reactThrottle from './R095_reactThrottle';
-import floatingPopulationList from './Floating_population/floatingPopulationList';
-import rechartsSimpleLineChart from './Floating_population/rechartsSimpleLineChart';
-import floatingPopulationListChart from './Floating_population/floatingPopulationListChart';
-import floatingPopulationAreaChart from './Floating_population/floatingPopulationAreaChart';
-import floatingPopulationBarChart from './Floating_population/floatingPopulationBarChart';
-import floatingPopulationComposedChart from './Floating_population/floatingPopulationComposedChart';
-import floatingPopulationScatterChart from './Floating_population/floatingPopulationScatterChart';
+import reactProxy from './R109_reactProxy';
 
 class App extends Component {
   render () {
@@ -28,14 +21,7 @@ class App extends Component {
       <div className="App">
         <HeaderAdmin/> 
         <Route exact path='/' component={LoginForm} />
-        <Route exact path='/Throttle' component={reactThrottle} />
-        <Route path='/floatPopulationList' component={floatingPopulationList} />
-        <Route path='/rechartsSimpleLineChart' component={rechartsSimpleLineChart} />
-        <Route path='/floatingPopulationListChart' component={floatingPopulationListChart} />
-        <Route path='/floatingPopulationAreaChart' component={floatingPopulationAreaChart} />
-        <Route path='/floatingPopulationBarChart' component={floatingPopulationBarChart} />
-        <Route path='/floatingPopulationComposedChart' component={floatingPopulationComposedChart} />
-        <Route path='/floatingPopulationScatterChart' component={floatingPopulationScatterChart} />
+        <Route exact path='/reactProxy' component={reactProxy} />
         <Footer/>
       </div>
     );
